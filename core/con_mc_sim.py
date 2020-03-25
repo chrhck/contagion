76ba81b1d9e2b306cbdfa4b050a71f96c0c0a994
aturcati <andrea.turcati@tum.de>
2020-03-25 15:17:43 +0100

    Also Susceptible can meet Infectious

--- core/con_mc_sim.py
+++ core/con_mc_sim.py
@@ -11,6 +11,7 @@ import numpy as np
 from time import time
 from numpy.random import choice
 
+
 class CON_mc_sim(object):
     """
     class: CON_mc_sim
@@ -56,26 +57,28 @@ class CON_mc_sim(object):
         self.__log = log
         self.__config = config
         self.__infect = infection
-        self.__dt = config['time step']
-        self.__t = np.arange(
-            0., self.__config['simulation length'],
-            step=self.__dt
-        )
-        self.__log.debug('The interaction intensity pdf')
-        if self.__config['interaction intensity'] == 'uniform':
+        self.__dt = config["time step"]
+        self.__t = np.arange(0.0, self.__config["simulation length"], step=self.__dt)
+        self.__log.debug("The interaction intensity pdf")
+        if self.__config["interaction intensity"] == "uniform":
             self.__intense_pdf = self.__intens_pdf_uniform
             # The Reproductive Number
             self.__R = (
-                self.__config['mean social circle interactions'] *
-                self.__config['infection duration mean'] * 0.5
+                self.__config["mean social circle interactions"]
+                * self.__config["infection duration mean"]
+                * 0.5
             )
         else:
-            self.__log.error('Unrecognized intensity pdf! Set to ' +
-                             self.__config['interaction intensity'])
-            exit('Check the interaction intensity in the config file!')
-        self.__log.debug('Constructing simulation population')
-        self.__log.debug('The infected ids and durations...')
-        infect_id = np.random.choice(range(len(population)), size=infected, replace=False)
+            self.__log.error(
+                "Unrecognized intensity pdf! Set to "
+                + self.__config["interaction intensity"]
+            )
+            exit("Check the interaction intensity in the config file!")
+        self.__log.debug("Constructing simulation population")
+        self.__log.debug("The infected ids and durations...")
+        infect_id = np.random.choice(
+            range(len(population)), size=infected, replace=False
+        )
         infect_dur = np.around(self.__infect.pdf_duration(infected).flatten())
         # Constructing population array
         # Every individual has 5 components
@@ -85,27 +88,30 @@ class CON_mc_sim(object):
         #   -infected
         #   -remaining duration of infection
         #   -immune
-        self.__log.debug('Filling the population array')
-        self.__population = np.array([
-            [i,                 # The person's id
-             population[i][0],  # The social circle
-             population[i][1],  # The encouter rate
-             False,             # Infected?
-             0.,                # Infection duration
-             False              # Immunity?
+        self.__log.debug("Filling the population array")
+        self.__population = np.array(
+            [
+                [
+                    i,  # The person's id
+                    population[i][0],  # The social circle
+                    population[i][1],  # The encouter rate
+                    False,  # Infected?
+                    0.0,  # Infection duration
+                    False,  # Immunity?
+                ]
+                for i in range(len(population))
             ]
-            for i in range(len(population))
-        ])
+        )
         # Adding the infected
         for i, id_inf in enumerate(infect_id):
             self.__population[id_inf][3] = True
             self.__population[id_inf][4] = infect_dur[i]
-        self.__log.info('There will be %d simulation steps' %len(self.__t))
+        self.__log.info("There will be %d simulation steps" % len(self.__t))
         # Removing social mobility of tracked people
         if tracked is not None:
             for i in tracked:
                 self.__population[i][2] = 0
-        if self.__config['save population']:
+        if self.__config["save population"]:
             self.__log.debug("Saving the distribution of infected")
             self.__distribution = []
             self.__total_infections = []
@@ -115,7 +121,7 @@ class CON_mc_sim(object):
         start = time()
         self.__simulation()
         end = time()
-        self.__log.info('MC simulation took %f seconds' % (end-start))
+        self.__log.info("MC simulation took %f seconds" % (end - start))
 
     @property
     def population(self):
@@ -220,25 +226,25 @@ class CON_mc_sim(object):
         """
         self.__infections = []
         for _ in self.__t:
-            #TODO: This needs to be optimized to a comprehension
+            # TODO: This needs to be optimized to a comprehension
             new_infections = []
             for person_id, person in enumerate(self.__population):
+                # If infected
                 if person[3]:
                     # Infected people contacts others
                     contacts = choice(person[1], size=person[2])
                     # Intensity of the contacts
                     intens_arr = self.__intense_pdf(len(contacts)).flatten()
-                    infection_prob = np.array([
-                        self.__infect.pdf(intens)
-                        for intens in intens_arr
-                    ])
+                    infection_prob = np.array(
+                        [self.__infect.pdf(intens) for intens in intens_arr]
+                    )
                     # Are they infected?
                     infection = np.around(infection_prob)
                     new_infections.append(
                         [
                             contacts[i]
                             for i in range(len(infection))
-                            if infection[i] > 0.
+                            if infection[i] > 0.0
                         ]
                     )
                     # subtracting 1 from the infection duration
@@ -248,25 +254,47 @@ class CON_mc_sim(object):
                         if self.__population[person_id][4] == 0:
                             self.__population[person_id][3] = False
                             self.__population[person_id][5] = True
+                # If Susceptible
+                elif not person[5]:
+                    # Susceptible person others
+                    contacts = choice(person[1], size=person[2])
+                    # Are there any infected?
+                    n_inf_cont = np.sum(
+                        [
+                            person[3]
+                            for person in self.__population
+                            if (person[0] in contacts)
+                        ]
+                    )
+                    if n_inf_cont > 0:
+                        # Intensity of the contacts
+                        intens_arr = self.__intense_pdf(n_inf_cont).flatten()
+                        infection_prob = np.array(
+                            [self.__infect.pdf(intens) for intens in intens_arr]
+                        )
+                        # Are they infected?
+                        infection = np.around(infection_prob)
+                        if np.any(infection):
+                            new_infections.append([person_id])
             # Adding newly infected to the population
             new_infection_counter = 0
             for new_infect in new_infections:
                 # The durations
-                tmp_dur = np.around(self.__infect.pdf_duration(len(new_infect)).flatten())
+                tmp_dur = np.around(
+                    self.__infect.pdf_duration(len(new_infect)).flatten()
+                )
                 for i, id_infect in enumerate(new_infect):
-                    if((not self.__population[id_infect][3]) &
-                       (not self.__population[id_infect][5])):
+                    if (not self.__population[id_infect][3]) & (
+                        not self.__population[id_infect][5]
+                    ):
                         self.__population[id_infect][3] = True
                         self.__population[id_infect][4] = tmp_dur[i]
                         new_infection_counter += 1
-            if self.__config['save population']:
-                self.__distribution.append(np.copy(
-                    self.__population
-                ))
+            if self.__config["save population"]:
+                self.__distribution.append(np.copy(self.__population))
                 self.__total_infections.append(len(self.__fetch_infected()))
                 self.__new_infections.append(new_infection_counter)
                 self.__immune.append(len(self.__fetch_immune()))
-
 
     def __intens_pdf_uniform(self, contacts):
         """
@@ -280,7 +308,7 @@ class CON_mc_sim(object):
             -np.array res:
                 The contact intensities
         """
-        return np.random.uniform(low=0., high=1., size=contacts)
+        return np.random.uniform(low=0.0, high=1.0, size=contacts)
 
     def __fetch_infected(self):
         """
@@ -292,10 +320,7 @@ class CON_mc_sim(object):
             -np.array res:
                 The number of currently infected
         """
-        return np.array([
-            person for person in self.__population
-            if person[3]
-        ])
+        return np.array([person for person in self.__population if person[3]])
 
     def __fetch_immune(self):
         """
@@ -307,8 +332,4 @@ class CON_mc_sim(object):
             -np.array res:
                 The number of currently infected
         """
-        return np.array([
-            person for person in self.__population
-            if person[5]
-        ])
-        
+        return np.array([person for person in self.__population if person[5]])
