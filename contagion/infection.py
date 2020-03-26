82113e611e1e3c566689a64f7a7d88879ff6a53a
Christian Haack <chr.hck@gmail.com>
2020-03-26 11:46:08 +0100

    Added interface for PDFs ; made config global.

--- contagion/infection.py
+++ contagion/infection.py
@@ -7,9 +7,10 @@ Constructs the infection.
 """
 
 # Imports
-import numpy as np
-# A truncated normal continuous random variable
-from scipy.stats import truncnorm
+import numpy as np  # type: ignore
+from .pdfs import TruncatedNormal, NormalizedProbability
+from .config import config
+
 
 class Infection(object):
     """
@@ -17,29 +18,25 @@ class Infection(object):
     Constructs the infection object
     Parameters:
         -obj log:
-            The logger
-        -dic config:
-            The configuration dictionary
+            The logger       
     Returns:
         -None
     """
-    def __init__(self, log, config):
+    def __init__(self, log):
         """
         function: __init__
         Initializes the infection object
         Parameters:
             -obj log:
                 The logger
-            -dic config:
-                The configuration dictionary
         Returns:
             -None
         """
         # TODO: Set up standard parameters for different diseases, which
         #   can be loaded by only setting the disease
         self.__log = log.getChild(self.__class__.__name__)
-        self.__config = config
-        if self.__config['random state'] is None:
+
+        if config['random state'] is None:
             self.__log.warning("No random state given, constructing new state")
             self.__rstate = np.random.RandomState()
         else:
@@ -47,22 +44,30 @@ class Infection(object):
 
         self.__log.debug('The infection probability pdf')
         if config['infection probability pdf'] == 'intensity':
-            self.__pdf = self.__pdf_intensity
+            self.__pdf_infection_prob = NormalizedProbability(0, 1)
         else:
             self.__log.error('Unrecognized infection pdf! Set to ' +
-                             self.__config['infection probability pdf'])
+                             config['infection probability pdf'])
             exit('Check the infection probability pdf in the config file!')
 
         self.__log.debug('The infection duration and incubation pdf')
-        if self.__config['infection duration pdf'] == 'gauss':
-            self.__pdf_duration = self.__pdf_duration_norm
+        if config['infection duration pdf'] == 'gauss':
+
+            dur_pdf = TruncatedNormal(
+                0,
+                np.inf,
+                config['infection duration mean'],
+                config['infection duration variance']
+                )
+            self.__pdf_duration = dur_pdf.rvs
+
         else:
             self.__log.error('Unrecognized infection duration pdf! Set to ' +
-                             self.__config['infection duration pdf'])
+                             config['infection duration pdf'])
             exit('Check the infection duration pdf in the config file!')
 
     @property
-    def pdf(self):
+    def pdf_infection_prob(self):
         """
         function: pdf
         The infection probability
@@ -73,7 +78,7 @@ class Infection(object):
                 The infection probability
                 Takes the sc intensity
         """
-        return self.__pdf
+        return self.__pdf_infection_prob
 
     @property
     def pdf_duration(self):
@@ -100,45 +105,4 @@ class Infection(object):
             -int length:
                 The duration of immunity
         """
-        return self.__config['immunity duration']
-
-
-    def __pdf_intensity(self, intensity):
-        """
-        function: __pdf_intensity
-        The infection probability is equal to the intensity of the interaction
-        Parameters:
-            -float intensity:
-                The social interaction intensity
-        Returns:
-            -float prob:
-                The infection probability
-        """
-        prob = intensity
-        return prob
-
-    def __pdf_duration_norm(self, infected):
-        """
-        function: __pdf_duration_norm
-        The normal distribution for the length of infection
-        Parameters:
-            -int infected:
-                The number of infected people
-        Returns:
-            -np.array duration:
-                The length of their infection
-        """
-
-        mean = self.__config['infection duration mean']
-        scale = self.__config['infection duration variance']
-
-        # Minimum social circle size is 0
-        a, b = (0 - mean) / scale, (np.inf - mean) / scale
-
-        # could also use binomial here
-        duration = truncnorm.rvs(
-            a, b, loc=mean, scale=scale, size=infected,
-            random_state=self.__rstate)
-        # duration = np.asarray(duration, dtype=int)
-
-        return duration
+        return config['immunity duration']
